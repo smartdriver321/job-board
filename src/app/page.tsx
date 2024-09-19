8023770cc022c12c2dc569adf95d584fe0dc2784
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-09-19 11:44:51 +0700

    Navbar, footer, metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import { Metadata } from 'next'
+
 import { JobFilterValues } from '@/lib/validation'
 import JobFilterSidebar from '@/components/JobFilterSidebar'
 import JobResults from '@/components/JobResults'
+import H1 from '@/components/ui/h1'
 
 interface PageProps {
 	searchParams: {
@@ -9,6 +12,33 @@ interface PageProps {
 		location?: string
 		remote?: string
 		page?: string
+	}
+}
+
+function getTitle({ q, type, location, remote }: JobFilterValues) {
+	const titlePrefix = q
+		? `${q} jobs`
+		: type
+			? `${type} developer jobs`
+			: remote
+				? 'Remote developer jobs'
+				: 'All developer jobs'
+
+	const titleSuffix = location ? ` in ${location}` : ''
+
+	return `${titlePrefix}${titleSuffix}`
+}
+
+export function generateMetadata({
+	searchParams: { q, type, location, remote },
+}: PageProps): Metadata {
+	return {
+		title: `${getTitle({
+			q,
+			type,
+			location,
+			remote: remote === 'true',
+		})} | Flow Jobs`,
 	}
 }
 
@@ -25,9 +55,7 @@ export default async function HomePage({
 	return (
 		<main className='m-auto my-10 max-w-5xl space-y-10 px-3'>
 			<div className='space-y-5 text-center'>
-				<h1 className='sm:text-4xl lg: text-5xl font-extrabold tracking-tight '>
-					Developer Jobs
-				</h1>
+				<H1>{getTitle(filterValues)}</H1>
 				<p className='text-muted-foreground'>Find your dream job.</p>
 			</div>
 			<section className='flex flex-col gap-4 md:flex-row'>
