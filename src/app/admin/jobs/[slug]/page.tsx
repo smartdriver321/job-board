172d18028cfa123ce2aae551d525e2451a108b77
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-09-19 19:01:13 +0700

    UseFormState (Approve/delete jobs)

---
+++ src/app/admin/jobs/[slug]/page.tsx
@@ -0,0 +1,24 @@
+import { notFound } from 'next/navigation'
+
+import prisma from '@/lib/prisma'
+import JobPage from '@/components/JobPage'
+import AdminSidebar from './AdminSidebar'
+
+interface PageProps {
+	params: { slug: string }
+}
+
+export default async function Page({ params: { slug } }: PageProps) {
+	const job = await prisma.job.findUnique({
+		where: { slug },
+	})
+
+	if (!job) notFound()
+
+	return (
+		<main className='m-auto my-10 flex max-w-5xl flex-col items-center gap-5 px-3 md:flex-row md:items-start'>
+			<JobPage job={job} />
+			<AdminSidebar job={job} />
+		</main>
+	)
+}
