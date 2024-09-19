fc9d6389c73bf2eee67ac43b406bef57daec1a48
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-09-19 18:50:40 +0700

    Clerk authentication (Admin dashboard) + middelware.ts

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,17 @@
+import { Metadata } from 'next'
+import { ClerkProvider } from '@clerk/nextjs'
+
+import AdminNavbar from './AdminNavbar'
+
+export const metadata: Metadata = {
+	title: 'Admin',
+}
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+	return (
+		<ClerkProvider>
+			<AdminNavbar />
+			{children}
+		</ClerkProvider>
+	)
+}
